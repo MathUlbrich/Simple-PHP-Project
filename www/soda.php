4ab93eace461fc72e3455c986d57871a95a66368
mathulbrich <mathulbrich@gmail.com>
2019-04-20 23:36:30 -0300

    Arquitetura com suporte a docker

--- www/soda.php
+++ www/soda.php
@@ -1,14 +1,12 @@
 <?php 
-
-	
-
+	$conn = null;	
 ?>
 
 <html>
 	
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="styles.css"></head>
+		<link rel="stylesheet" href="./styles.css"></head>
 	</head>
 	
 	<body>
